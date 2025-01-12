9223e1f0d253e391bf9ff6e3912da459646ad3d5
Squiggly-bit <anubhav1rajora@gmail.com>
2025-01-12 11:56:08 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# # Objectives:
+# We have learned few basics of python language
+# 
+# # Popular languages:
+# * Python
+# * Java
+# * C++
+
+# Some of the popular languages that Data Scientists use are:
+# 1) Python
+# 2) Java
+# 3) C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1) pandas
+# 2) numpy
+# 3) matplotlib
+
+# Data Science Tools
+# 
+# | Data Science Tools          |
+# |-----------------------------|
+# | Jupyter Notebook            |
+# | RStudio                     |
+# | Apache Zeppelin             |
+# 
+
+# Below are a few examples of evaluating arithmetic expressions in Python.
+# # Addition
+# result_add = 5 + 3
+# print("5 + 3 =", result_add)
+# 
+# # Subtraction
+# result_sub = 10 - 4
+# print("10 - 4 =", result_sub)
+# 
+# # Multiplication
+# result_mul = 6 * 7
+# print("6 * 7 =", result_mul)
+# 
+# # Division
+# result_div = 20 / 5
+# print("20 / 5 =", result_div)
+# 
+# # Floor Division
+# result_floordiv = 19 // 4
+# print("19 // 4 =", result_floordiv)
+# 
+# # Modulus
+# result_mod = 19 % 4
+# print("19 % 4 =", result_mod)
+# 
+# # Exponentiation
+# result_exp = 2 ** 3
+# print("2 ** 3 =", result_exp)
+# 
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers.
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60.
+200/60
+
+
+# ## Author
+# Anubhav Rajora
+
+# In[ ]:
+
+
+
+
